284c4c8049d3278674b8fb2d0135c8c49654b3f8
TiagoUniverse <tiagocesar68@gmail.com>
2022-10-14 10:44:48 -0300

    Trabalhando no modo offline dele. Não terminei ainda

--- sw.js
+++ sw.js
@@ -1,25 +1,94 @@
-// This is the "serving cached media" service worker
+const CACHE_NAME = `my-sample-app-cache-v1`;
 
-const CACHE = "pwabuilder-offline";
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      './index.html' ,
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+      './adicionar.html' ,
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+      './login.html' ,
+
+      './charts-chartjs.html' ,
+
+      './charts-echarts.html' ,
+
+      './dashboards.html' ,
+
+      './graficos.html' ,
+
+      './grf-teste.html' ,
+
+      './mapa.html' ,
+
+      './registrar.html' ,
+
+      './usuario.html' ,
+
+      './forms/contact.php' ,
+
+      './pwabuilder-sw/pwabuilder-sw-register.js' ,
+      
+      './pwabuilder-sw/pwabuilder-sw.js' ,
+      
+      'assets/css/cards.css' ,
+
+      'assets/css/style.css' ,
+
+      'assets/img/fotos/rio-1.png',
+      'assets/img/fotos/rio-2.png',
+      'assets/img/fotos/rio-3.png',
+
+      'assets/img/apple-touch-icon.png',
+      'assets/img/favicon.png',
+      'assets/img/inserir-img.png',
+      'assets/img/logo.png',
+
+      'assets/img/icons/29.png',
+      'assets/img/icons/40.png',
+      'assets/img/icons/57.png',
+      'assets/img/icons/58.png',
+      'assets/img/icons/60.png',
+      'assets/img/icons/80.png',
+      'assets/img/icons/114.png',
+      'assets/img/icons/120.png',
+      'assets/img/icons/180.png',
+      'assets/img/icons/1024.png',
+
+      'assets/img/icons/map-solid.svg',
+      'assets/img/icons/plus-solid.svg',
+      'assets/img/icons/user-solid.svg',
+    ]);
+  })());
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
 });
 
-workbox.loadModule('workbox-cacheable-response');
-workbox.loadModule('workbox-range-requests');
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
 
-workbox.routing.registerRoute(
-  /.*\.mp4/,
-  new workbox.strategies.CacheFirst({
-    cacheName: CACHE,
-    plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({statuses: [200]}),
-      new workbox.rangeRequests.RangeRequestsPlugin(),
-    ],
-  }),
-);
+    try {
+      // Try to fetch the resource from the network.
+      const fetchResponse = await fetch(event.request);
+
+      // Save the resource in the cache.
+      cache.put(event.request, fetchResponse.clone());
+
+      // And return it.
+      return fetchResponse;
+    } catch (e) {
+      // Fetching didn't work get the resource from the cache.
+      const cachedResponse = await cache.match(event.request);
+
+      // And return it.
+      return cachedResponse;
+    }
+  })());
+});
